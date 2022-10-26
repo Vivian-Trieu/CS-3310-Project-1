ab0fb7f7368275d1d758422a11856444f1732893
Vivian Trieu <vivian.dtrieu@gmail.com>
2022-10-25 17:10:00 -0700

    Classical Matrix implementation

---
+++ src/classicalMatrix.java
@@ -0,0 +1,54 @@
+
+
+public class classicalMatrix {
+    public static void main(String[] args) {
+        int n = 4;
+        int row1 = n, col1 = n, row2 = n, col2 = n;
+
+        int[][] firstMatrix = {{1, 1},
+                               {2, 2}};
+
+        int[][] secondMatrix = {{1, 1},
+                                {2, 2}};
+                               
+        multiplyMatrix(row1, col1, firstMatrix, row2, col2, secondMatrix);                       
+    }
+
+    public static void multiplyMatrix(
+        int row1, int col1, int[][] firstMatrix, 
+        int row2, int col2, int[][] secondMatrix) {
+    
+        int[][] product = new int[row1][col2]; // Matrix to store product
+
+        // Print First Matrix
+        System.out.println("\nFirst Matrix:");
+        printMatrix(firstMatrix, row1, col1);
+
+        // Print Second Matrix
+        System.out.println("\nSecond Matrix:");
+        printMatrix(secondMatrix, row2, col2);
+
+        // Multiply 2 Matrices
+        for (int i = 0; i < row1; i++){
+            for (int j = 0; j < col2; j++){
+                for (int k = 0; k < row2; k++){
+                    product[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
+                }
+            }
+        }
+
+        // Print Product
+        System.out.println("\nProduct of the Matrices:");
+        printMatrix(product, row1, col2);
+    }
+
+    // Method to print Matrix
+    public static void printMatrix(int M[][], int rowSize, int colSize){
+        for (int i = 0; i < rowSize; i++){
+            for (int j = 0; j < colSize; j++){
+                System.out.print(M[i][j] + " ");               
+            }
+            System.out.println();
+        }
+    }
+}
