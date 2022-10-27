7d0b976d21a9f2d69e28b7d08fa1bdb756f28a92
Vivian Trieu <vivian.dtrieu@gmail.com>
2022-10-27 00:39:33 -0700

    divideAndConquer implemented

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,73 +1,90 @@
 public class Matrix {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 2;
         
-        int[][] A = {{1, 1, 1, 1},
-                     {2, 2, 2, 2},
-                     {3, 3, 3, 3},
-                     {4, 4, 4, 4}};
+        int[][] A = {{1, 1},
+                     {2, 2}};
 
-        int[][] B = {{1, 1, 1, 1},
-                     {2, 2, 2, 2},
-                     {3, 3, 3, 3},
-                     {4, 4, 4, 4}};
+        int[][] B = {{1, 1},
+                     {2, 2}};
         
         // Print First Matrix
-        System.out.println("\nFirst Matrix:");
+        System.out.println("\nMatrix A:");
         printMatrix(A, n);
 
         // Print Second Matrix
-        System.out.println("\nSecond Matrix:");
+        System.out.println("\nMatrix B:");
         printMatrix(B, n);
 
         runClassicalMatrix(A, B, n);
-        runStrassenMatrix(A, B, n);                       
+        runStrassenMatrix(A, B, n);   
+        runDivideAndConquer(A, B, n);                    
     }
 
     // Classical Matrix  
     public static void runClassicalMatrix (int[][] A, int[][] B, int n) {
-        System.out.println("\nClassical Matrix Multiplication: ");
+        System.out.println("\nClassic: ");
         // Start timer
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
                          
         // Calculate Product Matrix
         classicalMatrix cMatrix = new classicalMatrix();                         
         int[][] C = cMatrix.multiplyMatrix(A, B);
 
+        // End timer
+        long endTime = System.nanoTime();
+
         // Print Product
         System.out.println("\nProduct of the Matrices:");
         printMatrix(C, n);
 
-        // End timer
-        long endTime = System.currentTimeMillis();
-
         // Calculate time to execute program
         long totalTime = endTime - startTime;
-        System.out.println("Total Time to Execute Program (in milliseconds): " + totalTime);
+        System.out.println("Total Time to Execute Program (in nanoseconds): " + totalTime);
     }
 
     // Divide and Conquer Matrix
+    public static void runDivideAndConquer (int[][] A, int[][] B, int n) {
+        System.out.println("\nDivide and Conquer: ");
+        // Start timer
+        long startTime = System.nanoTime();
+                         
+        // Calculate Product Matrix
+        divideAndConquer dncMatrix = new divideAndConquer();                         
+        int[][] C = dncMatrix.multiplyMatrix(A, B);
+
+        // End timer
+        long endTime = System.nanoTime();
+
+        // Print Product
+        System.out.println("\nProduct of the Matrices:");
+        printMatrix(C, n);
+
+        // Calculate time to execute program
+        long totalTime = endTime - startTime;
+        System.out.println("Total Time to Execute Program (in nanoseconds): " + totalTime);
+    }
 
     // Strassen’s matrix multiplication
     public static void runStrassenMatrix(int[][] A, int[][] B, int n) {
-        System.out.println("\nStrassen Matrix Multiplication: ");
+        System.out.println("\nStrassen's: ");
         // Start timer
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
 
         // Calculate Product Matrix
         strassenMatrix sMatrix = new strassenMatrix();
         int[][] C = sMatrix.multiplyMatrix(A, B);
 
+        // End timer
+        long endTime = System.nanoTime();
+
         // Print Product
         System.out.println("\nProduct of the Matrices:");
         printMatrix(C, n);
 
-        // End timer
-        long endTime = System.currentTimeMillis();
-
         // Calculate time to execute program
         long totalTime = endTime - startTime;
-        System.out.println("Total Time to Execute Program (in milliseconds): " + totalTime);
+        System.out.println("Total Time to Execute Program (in nanoseconds): " + totalTime);
     }
 
     // Method to print Matrix
--- src/divideAndConquer.java
+++ src/divideAndConquer.java
@@ -1,22 +1,13 @@
-public class strassenMatrix {
-    /**
-     * Strassen's Matrix Multiplication Implementation
-     * 
-     * @param A
-     * @param B
-     * @return Product Matrix C
-     */
-    public int[][] multiplyMatrix(int[][] A, int[][] B) {
-
+public class divideAndConquer {
+    public int[][] multiplyMatrix (int[][] A, int[][] B) {
+        
         int n = A.length;
 
         // Matrix to store product
         int[][] C = new int[n][n];
 
         if (n == 1) {
-
             C[0][0] = A[0][0] * B[0][0];
-
         } else {
             // Divide Matrix into parts by storing sub-parts into variables
             int[][] A11 = new int[n / 2][n / 2];
@@ -42,57 +33,28 @@ public class strassenMatrix {
 
             // Follow Algorithm
 
-            // M1 = (A11+A22)x(B11+B22)
-            int[][] M1 
-            = multiplyMatrix(add(A11, A22), add(B11, B22));
-
-            // M2 = (A21+A22)x(B11)
-            int[][] M2 
-            = multiplyMatrix(add(A21, A22), B11);
-
-            // M3 = (A11)x(B12-B22)
-            int[][] M3 
-            = multiplyMatrix(A11, sub(B12, B22));
-
-            // M4 = (A22)x(B21-B11)
-            int[][] M4 
-            = multiplyMatrix(A22, sub(B21, B11));
-
-            // M5 = (A11+A12)x(B22)
-            int[][] M5 
-            = multiplyMatrix(add(A11, A12), B22);
-
-            // M6 = (A21-A11)x(B11+B12)
-            int[][] M6 
-            = multiplyMatrix(sub(A21, A11), add(B11, B12));
-
-            // M7 = (A12-A22)x(B21+B22)
-            int[][] M7 
-            = multiplyMatrix(sub(A12, A22), add(B21, B22));
-
-            // C11 = M1 + M4 - M5 + M7
+            // C11 = (A11xB11)+(A12xB21)
             int[][] C11 
-            = add(sub(add(M1, M4), M5), M7);
+            = add(multiplyMatrix(A11, B11), multiplyMatrix(A12, B21));
 
-            // C12 = M3 + M5
-            int[][] C12 
-            = add(M3, M5);
+            // C12 = (A11xB12)+(A12xB22)
+            int[][] C12
+            = add(multiplyMatrix(A11, B12), multiplyMatrix(A12, B22));
 
-            // C21 = M2 + M4
-            int[][] C21 
-            = add(M2, M4);
+            // C21 = (A21xB11)+(A22xB21)
+            int[][] C21
+            = add(multiplyMatrix(A21, B11), multiplyMatrix(A22, B21));
 
-            // C22 = M1 - M2 + M3 + M6
-            int[][] C22 
-            = add(add(sub(M1, M2), M3), M6);
+            // C22 = (A21xB12)+(A22xB22)
+            int[][] C22
+            = add(multiplyMatrix(A21, B12), multiplyMatrix(A22, B22));
 
-            // Join 4 halves into one matrix
             join(C11, C, 0, 0);
             join(C12, C, 0, n / 2);
             join(C21, C, n / 2, 0);
             join(C22, C, n / 2, n / 2);
         }
-
+        
         return C;
     }
 
@@ -153,30 +115,6 @@ public class strassenMatrix {
             for (int j = 0; j < n; j++) {
 
                 C[i][j] = A[i][j] + B[i][j];
-            }
-        }
-        return C;
-    }
-
-    /**
-     * Function to subtract two matrices
-     * 
-     * @param A
-     * @param B
-     * @return Difference Matrix C
-     */
-    public int[][] sub(int[][] A, int[][] B) {
-
-        int n = A.length;
-
-        // Difference Matrix
-        int[][] C = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-
-            for (int j = 0; j < n; j++) {
-
-                C[i][j] = A[i][j] - B[i][j];
             }
         }
         return C;
